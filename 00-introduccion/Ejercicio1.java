2405a1c23d4c40c57d910bfe7af7515811b3148f
Myriam2704 <myriam.candelcano2704@gmail.com>
2026-03-09 01:01:28 +0100

    Add Ejercicio1.java to display personal information

---
+++ 00-introduccion/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+       // Ejercicio 1 Muestra por pantalla: tu nombre, tu edad, el nombre del ciclo que estudias
+        System.out.println("hola, me llamo Myriam");
+        System.out.println("tengo 35 años");
+        System.out.println("Estoy estudiando Daw");
+    }
+}
