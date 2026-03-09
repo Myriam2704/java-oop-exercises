19a85896b143808fac44302fa048cdd96f4c95e4
Myriam2704 <myriam.candelcano2704@gmail.com>
2026-03-09 01:02:24 +0100

    Add Ejercicio2.java with a greeting message

---
+++ 00-introduccion/Ejercicio2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        // Ejercicio 2 Muestra una especie de cartel:
+        System.out.println("*********************");
+        System.out.println("   MI PRIMER JAVA");
+        System.out.println("*********************");
+    }
+}
