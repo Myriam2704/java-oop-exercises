9eda173d50613371ee8c198d034d96494c960206
Myriam2704 <myriam.candelcano2704@gmail.com>
2026-03-09 01:03:14 +0100

    Add Ejercicio3.java with programming motivation messages

---
+++ 00-introduccion/Ejercicio3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+        // Ejercicio 3 Muestra tres frases, una en cada línea, sobre por qué quieres aprender programación.
+        System.out.println("Me gustaría aprender a programar porque me parece lo más interesante que he hecho nunca ");
+        System.out.println("Es para mi un gran reto, entender, saber y conocer todo lo que se puede hacer sabiendo programas");
+        System.out.println("Lo más importante es lo mucho que me gusta, pasan horas y ni lo noto cuando estoy aprendiendo a programr");
+
+    }
+}
